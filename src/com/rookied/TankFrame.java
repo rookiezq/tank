e87f30ed1b2ef765a6ae54292cfc979d699875d9
rookied <rookiezq@icloud.com>
2021-04-24 09:14:54 +0800

    添加键盘处理,根据方向键的按键状态判断坦克的移动方向

--- src/com/rookied/TankFrame.java
+++ src/com/rookied/TankFrame.java
@@ -36,24 +36,61 @@ public class TankFrame extends Frame {
      */
     @Override
     public void paint(Graphics g) {
-        System.out.println(1);
+        //System.out.println(1);
         g.fill3DRect(x, y, 50, 50, true);
-        x += 10;
+        //x += 10;
         //y += 10;
     }
 
     class MyKeyListener extends KeyAdapter {
+        boolean bL = false;
+        boolean bU = false;
+        boolean bR = false;
+        boolean bD = false;
+
         @Override
         public void keyPressed(KeyEvent e) {
-            System.out.println("按键按下");
-            x+=50;
+            int key = e.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_LEFT:
+                    bL = true;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU = true;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = true;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = true;
+                    break;
+                default:
+                    break;
+            }
             //自动调用paint()
             //repaint();
         }
 
         @Override
         public void keyReleased(KeyEvent e) {
-            System.out.println("按键抬起");
+            //System.out.println("按键抬起");
+            int key = e.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_LEFT:
+                    bL = false;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU = false;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = false;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = false;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
